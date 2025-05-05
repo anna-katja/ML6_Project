07ff35ee4d8bdd3ad347e36f6fb688ce91de95e5
ML6 User <liad.magen@edu.fh-campuswien.ac.at>
2025-05-05 17:29:53 +0000

    added file with preprocessing functions

---
+++ src/preprocessing.py
@@ -0,0 +1,66 @@
+from datasets import load_dataset, DatasetDict
+import re
+import contractions
+from nltk.corpus import stopwords
+import string
+
+def custom_dataset_size(dataset, size, split_ratio=(0.7, 0.15, 0.15)):
+    assert isinstance(dataset, DatasetDict), "Input dataset must be a DatasetDict"
+    assert isinstance(size, int), "Size must be an integer"
+    assert size > 0, "Size must be greater than 0"
+    assert len(split_ratio) == 3, "Split ratio must have 3 elements"
+    assert abs(sum(split_ratio) - 1.0) < 1e-6, "Split ratio must sum to 1"
+
+    train_size = round(size * split_ratio[0])
+    val_size = round(size*(split_ratio[0]+split_ratio[1]))
+    test_size = round(size*split_ratio[2])
+
+    subset = DatasetDict(
+        train = dataset["train"].shuffle(seed=42).select(range(train_size)),
+        val = dataset["train"].shuffle(seed=42).select(range(train_size, val_size)),
+        test = dataset["test"].shuffle(seed=42).select(range(test_size))
+    )
+
+    return subset
+
+# patterns of metadata that needs to be removed
+
+pattern_1 = r"^[^(]*\([^\)]*\)\s*--\s*"
+pattern_2 = r"^.*UPDATED:\s+\.\s+\d{2}:\d{2}\s+\w+,\s+\d+\s+\w+\s+\d{4}\s+\.\s+"
+pattern_3 = r"^By\s+\.\s+[A-Z][a-z]+\s+[A-Z][a-z]+\s+\.\s+(?:and\s+Associated\s+Press\s+Reporter\s*\.\s*)?"
+pattern_4 = r"^(\([^\)]*\))"
+pattern_5 = r"^By\s+\.\s+[A-Z][a-z]+\s+[A-Z][a-z]+\s+\.\s+PUBLISHED:\s+\.\s+\d{2}:\d{2}\s+EST,\s+\d{1,2}\s+[A-Za-z]+\s+\d{4}\s+\.\s+\|\s+\.\s+UPDATED:\s+\.\s+\d{2}:\d{2}\s+EST,\s+\d{1,2}\s+[A-Za-z]+\s+\d{4}\s+\.\s+"
+
+metadata_patterns = [pattern_1, pattern_2, pattern_3, pattern_4, pattern_5]
+
+def minimal_preprocessing(data):
+    # removing metadata
+    for pattern in metadata_patterns:
+        data["article"] = re.sub(pattern, '', data["article"])
+    # handling contractions
+    data["article"] = contractions.fix(data["article"])
+    data["highlights"] = contractions.fix(data["highlights"])
+    # removing extraneous white space
+    data["article"] = data["article"].strip()
+    data["highlights"] = data["highlights"].strip()
+
+    return data
+
+# other functions for preprocessing that could be called
+def lowercase(data):
+    data["article"] = data["article"].lower()
+    #data["highlights"] = data["highlights"].lower()
+    return data
+
+# outcomment if necessary
+# nltk.download("stopwords")
+def remove_stopwords(data):
+    stop_words = set(stopwords.words("english"))
+    data["article"] = " ".join([word for word in data["article"].split() if word not in stop_words])
+    #data["highlights"] = " ".join([word for word in data["highlights"].split() if word not in stop_words])
+    return data
+
+def remove_punctuation(data):
+    data["article"] = data["article"].translate(str.maketrans("", "", string.punctuation))
+    #data["highlights"].translate(str.maketrans("", "", string.punctuation))
+    return data
